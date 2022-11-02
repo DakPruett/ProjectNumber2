d905d59b0079e052cc1cde04126d9225faf7db45
dakwpruett <dakwpruett@gmail.com>
2022-11-02 14:47:00 -0400

    Update

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const http = require('http');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
@@ -35,6 +40,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+    io.on('connection', (socket) => {
+        socket.on('chat message', (msg) => {
+            io.emit('chat message', msg);
+        });
+    });
+});
+
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    server.listen(PORT, () => console.log('Now listening'));
 });

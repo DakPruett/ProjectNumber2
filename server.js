853447aaf825f9da842055014da75c51b0098645
dakwpruett <dakwpruett@gmail.com>
2022-11-02 17:23:29 -0400

    update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: '',
     cookie: {},
     resave: false,
     saveUninitialized: true,
